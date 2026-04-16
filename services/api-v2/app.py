3885e11fb31c931475b04754dce56c7000fddcbb
Людмила <mila@MacBook-Pro-Ludmila.local>
2026-04-16 14:12:25 +0300

    load balancing

--- services/api-v2/app.py
+++ services/api-v2/app.py
@@ -1,7 +1,7 @@
 import os
 import socket
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 def create_app() -> Flask:
@@ -16,6 +16,29 @@ def create_app() -> Flask:
             service=service_version,
             color=service_color,
             host=socket.gethostname(),
+        )
+
+    @app.get("/info")
+    def info():
+        return jsonify(
+            version=service_version,
+            color=service_color,
+            host=socket.gethostname(),
+        )
+
+    @app.route("/echo", methods=["GET", "POST"])
+    def echo():
+        return jsonify(
+            method=request.method,
+            version=service_version,
+            headers={
+                "X-Real-IP": request.headers.get("X-Real-IP"),
+                "X-Forwarded-For": request.headers.get("X-Forwarded-For"),
+                "X-Forwarded-Proto": request.headers.get("X-Forwarded-Proto"),
+                "X-API-Version": request.headers.get("X-API-Version"),
+            },
+            args=request.args,
+            json=request.get_json(silent=True),
         )
 
     @app.get("/health")
